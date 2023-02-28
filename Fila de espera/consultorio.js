ba3b69add4d4db7cee4823e97252d54ca8e9d74d
Matheus Ortiz <math.ortiz@outlook.com>
2023-02-28 19:51:28 -0300

    Fila

---
+++ Fila de espera/consultorio.js
@@ -0,0 +1,33 @@
+let fila = [ ]
+let opcao = ""
+
+do {
+    let pacientes = ""
+    for (let i = 0; 1 < fila.length; i++){
+        pacientes += (i + 1) + "º - " + fila[i] + "\n"
+    }
+
+    opcao = prompt(
+        "Pacientes:\n" + pacientes +
+        "\nEscolha uma ação:\n1. Novo paciente\n2. Consultar paciente\n3. Sair"
+    )
+
+    switch(opcao){
+        case "1":
+            const novoPaciente = prompt("Qual é o nome do paciente?")
+            fila.push(novoPaciente)
+            break
+        case "2":
+            const pacienteConsultado = fila.shift()
+            if (pacienteConsultado) {
+                alert(pacienteConsultado + " foi removido da fila.")
+            } else {
+                ("Não há pacientes na fila!")
+            }
+            break
+        case "3":
+            alert("Encerrando...")
+        default:
+            alert("Opção inválida!")
+    }
+} while (opcao !== "3")
