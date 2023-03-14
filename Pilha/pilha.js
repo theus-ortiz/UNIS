11291d8722ce91ce5de3b31142edaa00ac18cbe6
Matheus Ortiz <math.ortiz@outlook.com>
2023-03-13 21:21:38 -0300

    Pilha

---
+++ Pilha/pilha.js
@@ -0,0 +1,44 @@
+class Stack {
+    constructor() {
+      this.items = [];
+    }
+  
+    push(element) {
+      this.items.push(element);
+    }
+  
+    pop() {
+      if (this.items.length === 0) {
+        return "Underflow";
+      }
+      return this.items.pop();
+    }
+  
+    peek() {
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    printStack() {
+      let str = "";
+      for (let i = 0; i < this.items.length; i++) {
+        str += this.items[i] + " ";
+      }
+      return str;
+    }
+}
+  
+let stack = new Stack();
+stack.push("carta1");
+stack.push("carta2");
+stack.push("carta3");
+  
+console.log(stack.printStack());
+  
+stack.pop();
+  
+console.log(stack.printStack());
+  
